36923e4f58dc8696a3456f28b52efc3e642687bd
JEmbrace <1623143672@qq.com>
2020-10-20 11:09:46 +0800

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test">
+    <h1>Test</h1>
+    <h2>Welcome to Test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Test Components'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
